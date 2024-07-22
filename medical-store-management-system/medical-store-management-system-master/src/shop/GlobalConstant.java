b6e63e650242c0894198ea49f0f03ba17c048cb7
Diyora <cekhushdiyora@gmail.com>
2024-07-22 21:44:46 +0530

    Initial commit

---
+++ medical-store-management-system/medical-store-management-system-master/src/shop/GlobalConstant.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shop;
+
+
+public class GlobalConstant {
+    public static int userType;
+    public static String name;
+}
